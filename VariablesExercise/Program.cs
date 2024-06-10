3219099172aa71b6ffbf55cde3bcc485c6a4e8fb
Shayn Coffey <Shayncoffey30@gmail.com>
2024-06-10 11:41:22 -0400

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -2,9 +2,27 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello, World!");
+            string dogName; //Declaration syntax
+
+            dogName = "Rain"; //Initialized ---assigning a value
+
+            int dogAge = 5; //Declaring and Initializing
+
+            char firstInitial = 'R';
+
+            bool isHungry = true;
+
+            double dogWeight = 54.3;
+
+            decimal dogHeight = 14.7m;
+
+            Console.WriteLine($"Hello. My dog's name is {dogName} , his initial is {firstInitial}, and he is {dogAge} years old.");
+            Console.WriteLine($"He weighs {dogWeight} pounds and he is {dogHeight} inches tall.");
+            Console.WriteLine($"It is {isHungry} he is always hungry.");
+
+                    
         }
     }
 }
